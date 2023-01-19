13025f36bd053305105285db623d05acbecaa0e2
Cali-Newton <122839723+Cali-Newton@users.noreply.github.com>
2023-01-18 20:34:08 -0600

    Homework 1 code file

---
+++ Homework1.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework1;
+class Program
+{
+    //The code used for Homework1
+    static void Main(string[] args)
+    {
+       float x = 2.5F; // The value of x;
+       float y = 3.3F; // The value of y;
+       float z;
+       z = (4*x*x)+3*y; // Creating the equation to find z
+       Console.WriteLine("X = " + x + ", "+"Y = " + y);
+       Console.WriteLine("The value of Z is: "+z);
+    }
+}
