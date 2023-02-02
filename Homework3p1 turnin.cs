c7f951cacab7e7984c9956774206c575430706b3
Cali-Newton <122839723+Cali-Newton@users.noreply.github.com>
2023-02-01 18:41:25 -0600

    Add files via upload

---
+++ Homework3p1 turnin.cs
@@ -0,0 +1,28 @@
+﻿namespace Homework3p1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool nprime = true;
+        Console.WriteLine("Input an integer: "); //user inputs an integer
+        string input = Console.ReadLine();
+        int number = Convert.ToInt16(input);
+
+        for(int i=2; i<number/2;i++) // for loop that is checking for a prime number
+        {
+            if(number%i==0)
+            {
+                nprime = false;
+                break;
+            }
+        }
+        if(nprime) // if nprime is true then it will output prime, else number is non-prime
+        {
+            Console.WriteLine("N is Prime ");
+        }
+        else
+        {
+            Console.WriteLine("N is non-prime ");
+        }
+    }
+}
---
+++ Homework3p2 turnin.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework3p2;
+class Program
+{
+    static void Main(string[] args)
+    {
+     Console.WriteLine("Assign an int value to N: "); //Asking user to input a number
+        int n = Convert.ToInt16(Console.ReadLine());
+        
+        for(int row = 0;row < n;row++){ 
+            
+        for(int col = 0; col < n ;col++){
+            Console.Write("#");
+        }
+            Console.WriteLine();
+        }   
+    }
+}
