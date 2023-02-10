4cd29c063731e0bd0d61a38b1ea940fdc000b493
Cali-Newton <122839723+Cali-Newton@users.noreply.github.com>
2023-02-10 12:59:38 -0600

    Add files via upload

---
+++ HW4p2 turnnin.cs
@@ -0,0 +1,39 @@
+﻿namespace HW4p2;
+class Program
+{
+    static void pattern(int N,string shape){
+    //patern method having parameters N and shape
+    int i,j,k; //declaring variables to use
+    if(shape=="left"){
+    //if shape is left
+       for(i=1;i<=N;i++)
+       {
+    	for(j=1;j<=i;j++)
+    	   Console.Write("*"); //printing star
+    	Console.Write("\n"); //printing new line
+       }
+      }
+    else if(shape=="right"){
+    //if shape is right
+     for (i = 1; i <= N; i++)  
+         {  
+            for (j = 1; j <= N-i; j++)  
+            {Console.Write(" ");}  //printing space
+            for (k = 1; k <= i; k++)  
+            {  Console.Write("*");}  //printing star
+            Console.WriteLine("");  }  
+        Console.ReadLine();}
+    }
+    public static void Main() 
+{//main method
+   int N; //declaring variable
+   string shape; // declaring the shape
+   Console.Write("Input N: ");
+   N= Convert.ToInt32(Console.ReadLine()); // input the number of rows
+   Console.Write("Input shape: ");
+   shape= Console.ReadLine(); // input of the shape
+   Console.Write("N is: "+N+"; shape is "+shape+"\n"); //displaying N and shape
+   pattern(N,shape); //calling method
+}
+}
+
---
+++ Homework4p1 turnin.cs
@@ -0,0 +1,40 @@
+﻿namespace Homework4p1;
+class Program
+{
+    // function named largest that takes 2 Integers a and b
+// and return largest one
+
+static int largest(int a, int b){
+
+if(a>b){
+
+return a;
+
+}
+
+else if(b>a){
+
+return b;
+
+}
+
+else{
+
+return -1;
+
+}
+
+}
+
+static void Main() {
+
+int a = 3, b=5;
+
+// call the largest number
+
+int lar = largest( a, b);
+Console.WriteLine("\nLargest Number is : "+lar);
+}
+        }
+    
+
